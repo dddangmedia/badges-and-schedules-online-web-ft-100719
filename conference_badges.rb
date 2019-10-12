d443554949dde3bd6d422967b3fe7d4eac621536
Duong Dang <dddangmedia@gmail.com>
2019-10-12 02:28:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |message|
+    badge_messages.push(message)
+  end
+end
